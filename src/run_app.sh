a1e9de87d877ccc58ad9fe219ea4639d09bf3be1
srkadamb@cisco.com <srkadamb@cisco.com>
2022-05-01 13:07:08 +0530

    Testing

--- src/run_app.sh
+++ src/run_app.sh
@@ -3,6 +3,7 @@ pipenv install psycopg2-binary
 # migrate the database
 pipenv run flask db init
 pipenv run flask db revision --rev-id b7a1d28e08d0
+pipenv run flask db revision --rev-id b6bd18b9ebe7
 pipenv run flask db migrate
 pipenv run flask db upgrade
 
